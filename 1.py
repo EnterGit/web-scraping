efa215778346f43c0b98339a149128bf8bfeaf71
manumanoj0010 <manumanoj0010@gmail.com>
2019-03-28 10:18:47 +0530

    adding the 1st scrape

---
+++ 1.py
@@ -0,0 +1,23 @@
+import requests
+from fake_useragent import UserAgent
+import re
+from bs4 import BeautifulSoup
+import urllib
+
+
+url="https://www.consumerreports.org/cro/a-to-z-index/products/index.htm"
+
+#user_agent=UserAgent()
+
+#page=requests.get(url,headers={'user-agent':user_agent.chrome})
+
+page=urllib.request.urlopen(url)
+
+soup=BeautifulSoup(page,'lxml')
+
+element=soup.find_all('div',attrs={'class':'crux-body-copy'})
+
+products=[div.a.string for div in element]
+
+for items in products:
+    print(items)
